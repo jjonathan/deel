a40899172affaafd7ab650cc44e14174000ad325
Jonathan Machado <jonathan.mmachado@outlook.com>
2022-11-12 11:32:10 -0300

    Fixing contracts: using the profile_id on the query

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
+const { Op } = require("sequelize")
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
@@ -14,8 +15,21 @@ app.set('models', sequelize.models)
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const contract = await Contract.findOne({
+        where: {
+            [Op.and]: [
+                {id: id},
+                {
+                    [Op.or]: [
+                        {ClientId: req.get('profile_id')},
+                        {ContractorId : req.get('profile_id')}
+                    ]
+                }
+            ]
+        }
+    })
     if(!contract) return res.status(404).end()
+    if(!contract.profilie)
     res.json(contract)
 })
 module.exports = app;
